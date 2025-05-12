dfbbf9bfcccc06ae16c94c3c23530db76192ecea
Maciej Szostak <info@reco.studio>
2025-05-12 11:31:54 +0200

    Initial Commit

---
+++ RickAndMortyApi/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  RickAndMortyApi
+//
+//  Created by Maciej Szostak on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
