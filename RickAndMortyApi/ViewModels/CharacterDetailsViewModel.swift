d63dd37071bed153fe98d119ac2df1f4ae886365
Maciej Szostak <info@reco.studio>
2025-05-15 00:30:30 +0200

    Add ViewModels for list, details and episode screens

---
+++ RickAndMortyApi/ViewModels/CharacterDetailsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterDetailsViewModel.swift
+//  RickAndMortyApi
+//
+//  Created by Maciej Szostak on 15/05/2025.
+//
+
+import SwiftUI
+
+struct CharacterDetailsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterDetailsViewModel()
+}
---
+++ RickAndMortyApi/ViewModels/EpisodeDetailsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailsViewModel.swift
+//  RickAndMortyApi
+//
+//  Created by Maciej Szostak on 15/05/2025.
+//
+
+import SwiftUI
+
+struct EpisodeDetailsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeDetailsViewModel()
+}
